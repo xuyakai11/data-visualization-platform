42ef8c3ad83563b3651debd0ab97db63b8c15f9a
xuyakai <869790742@qq.com>
2019-01-04 17:24:28 +0800

    add declare

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/app',
-    Plugins: [
-      [ // 配置antd按需加载组件代码和样式的babel插件
-        "import",
-        { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
-      ]
+    '@vue/app'
+  ],
+  plugins: [
+    [
+      "import",
+      { libraryName: "ant-design-vue", libraryDirectory: "es", style: true }
     ]
   ]
 }
