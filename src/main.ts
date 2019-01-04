232f8f885872fc26a7112349336cfaa5f01bc956
lpc <m18797990285@163.com>
2019-01-04 16:57:08 +0800

    初步设置菜单页首页

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'ant-design-vue/dist/antd.css' // 引入antd样式
+//import 'ant-design-vue/lib/button' // 按需引入按钮样式
 import './axios.tool' // 导入封装好的axios
 import './registerServiceWorker'
 import './assets/common.css'
 
 Vue.config.productionTip = true
+
+Vue.use(Antd)
 
 new Vue({
   router,
